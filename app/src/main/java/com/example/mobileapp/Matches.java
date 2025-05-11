16e36b6b390252a97d4eecbb3c6320132ce97420
Kasir Ahmed Sayem <kasirahmed9999@outlook.com>
2025-05-11 17:59:40 +0200

    feat: create Match model and layout; rename Statistics.java to Matches.java

--- app/src/main/java/com/example/mobileapp/Matches.java
+++ app/src/main/java/com/example/mobileapp/Matches.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Statistics extends AppCompatActivity {
+public class Matches extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_statistics);
+        setContentView(R.layout.activity_matches);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
